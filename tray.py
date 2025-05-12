3a7e856a5f4e45906e6ef707fc76d6cb1dbcc3a1
leapbtw <36914991+leapbtw@users.noreply.github.com>
2025-05-12 16:57:52 +0200

    typooooooooo

--- tray.py
+++ tray.py
@@ -14,7 +14,7 @@ else:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Correct file paths
-TRAY_ICON_PATH = os.path.join(BASE_DIR, "tray.ico")
+TRAY_ICON_PATH = os.path.join(BASE_DIR, "icon.ico")
 UXPLAY_PATH = os.path.join(BASE_DIR, "bin", "uxplay.exe")
 IMAGE = PIL.Image.open(TRAY_ICON_PATH)
 
